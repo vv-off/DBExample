0d8380b642e0c85eb41fabe3cdadc331e3704237
vv-off <vv-off83@mail.ru>
2016-08-10 21:10:33 +0300

    Реализован класс DataBaseHandler

--- app/src/main/java/com/github/vv_off/dbexample/DataBaseHandler.java
+++ app/src/main/java/com/github/vv_off/dbexample/DataBaseHandler.java
@@ -1,10 +1,13 @@
 package com.github.vv_off.dbexample;
 
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataBaseHandler extends SQLiteOpenHelper implements IDataBaseHandler{
@@ -36,36 +39,81 @@ public class DataBaseHandler extends SQLiteOpenHelper implements IDataBaseHandle
 
     @Override
     public DataDB getDataDB(int id) {
-        return null;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(DATABASE_TABLE_NAME, new String[] {KEY_ID,
+                                    KEY_HEADER,KEY_MESSAGE}, KEY_ID + "=?",
+                                    new String[] {String.valueOf(id)}, null, null, null, null);
+        if(cursor != null){
+            cursor.moveToFirst();
+        }
+        DataDB dataDB = new DataDB(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2));
+        return dataDB;
     }
 
     @Override
     public List<DataDB> getAllDataDB() {
-        return null;
+        List<DataDB>  dataDBList = new ArrayList<DataDB>();
+        String selectQuery = "SELECT  * FROM " + DATABASE_TABLE_NAME;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if(cursor.moveToFirst()){
+            do{
+                DataDB dataDB = new DataDB();
+                dataDB.setID(Integer.parseInt(cursor.getString(0)));
+                dataDB.setHeader(cursor.getString(1));
+                dataDB.setMessage(cursor.getString(2));
+                dataDBList.add(dataDB);
+            }while(cursor.moveToNext());
+        }
+        return dataDBList;
     }
 
     @Override
-    public int countDataDB() {
-        return 0;
+    public int getCountDataDB() {
+        String countQuery = "SELECT  * FROM " + DATABASE_TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
+
+        return cursor.getCount();
     }
 
     @Override
     public void addDataDB(DataDB dataDB) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_HEADER,dataDB.getHeader());
+        values.put(KEY_MESSAGE,dataDB.getMessage());
 
+        db.insert(DATABASE_TABLE_NAME,null,values);
+        db.close();
     }
 
     @Override
     public int updateDataDB(DataDB dataDB) {
-        return 0;
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_HEADER, dataDB.getHeader());
+        values.put(KEY_MESSAGE, dataDB.getMessage());
+
+        return db.update(DATABASE_TABLE_NAME, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(dataDB.getID()) });
     }
 
     @Override
     public void deleteDataDB(DataDB dataDB) {
-
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(DATABASE_TABLE_NAME, KEY_ID + " = ?", new String[] { String.valueOf(dataDB.getID()) });
+        db.close();
     }
 
     @Override
     public void deleteAll() {
-
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(DATABASE_TABLE_NAME,null,null);
+        db.close();
     }
 }
--- app/src/main/java/com/github/vv_off/dbexample/IDataBaseHandler.java
+++ app/src/main/java/com/github/vv_off/dbexample/IDataBaseHandler.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface IDataBaseHandler {
     public DataDB getDataDB(int id);
     public List<DataDB> getAllDataDB();
-    public int countDataDB();
+    public int getCountDataDB();
     public void addDataDB(DataDB dataDB);
     public int updateDataDB(DataDB dataDB);
     public void deleteDataDB(DataDB dataDB);
