158c7a5fc5c22d54ef683e73139cb716fcf5fab7
vv-off <vv-off83@mail.ru>
2016-08-15 18:26:51 +0300

    Вывод ListView

--- app/src/main/java/com/github/vv_off/dbexample/MainActivity.java
+++ app/src/main/java/com/github/vv_off/dbexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.github.vv_off.dbexample;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,16 +10,25 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     ListView mListViewMessage;
     Button mBtnAdd;
     DataBaseHandler dbHandler = new DataBaseHandler(this);
 
+
+
+    String[] data = {"Строка1", "Строка1", "Строка1", "Строка1"};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         mListViewMessage = (ListView)findViewById(R.id.listViewMessage);
 
@@ -40,12 +50,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBtnAdd = (Button)findViewById(R.id.btnAdd);
         mBtnAdd.setOnClickListener(this);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,dbHandler.getAllDataDB());
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,getAllHeaders());
         mListViewMessage.setAdapter(adapter);
     }
 
     @Override
     public void onClick(View v) {
         //код нажатия на кнопку
+        dbHandler.addDataDB(new DataDB("Заголовок", "Текст записи"));
+    }
+
+    public List<String> getAllHeaders(){
+        List<String> listHeader = new LinkedList<>();
+        for(int i=0;i<dbHandler.getAllDataDB().size();i++){
+            listHeader.add(i,dbHandler.getAllDataDB().get(i).getHeader());
+        }
+        return listHeader;
     }
 }
